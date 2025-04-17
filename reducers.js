62f4a2bacc9d95d589ecde568017621a892a140d
Omar8177 <omxr60@icloud.com>
2025-04-17 10:52:06 -0700

    Update reducers.js

--- reducers.js
+++ reducers.js
@@ -1,1 +1,25 @@
 
+const cartReducer = (state = [], action) => {
+  switch (action.type) {
+    case "ADD_TO_CART":
+      return [...state, { ...action.payload, quantity: 1 }];
+    case "REMOVE_FROM_CART":
+      return state.filter((plant) => plant.id !== action.payload);
+    case "INCREASE_QUANTITY":
+      return state.map((plant) =>
+        plant.id === action.payload
+          ? { ...plant, quantity: plant.quantity + 1 }
+          : plant
+      );
+    case "DECREASE_QUANTITY":
+      return state.map((plant) =>
+        plant.id === action.payload && plant.quantity > 1
+          ? { ...plant, quantity: plant.quantity - 1 }
+          : plant
+      );
+    default:
+      return state;
+  }
+};
+
+export default cartReducer;

b1bfdb639b853f47cd8d0dcb6c1db6e2d92a1df0
Omar8177 <omxr60@icloud.com>
2025-04-17 10:51:48 -0700

    Update actions.js

--- actions.js
+++ actions.js
@@ -1,1 +1,20 @@
 
+export const addToCart = (plant) => ({
+  type: "ADD_TO_CART",
+  payload: plant,
+});
+
+export const removeFromCart = (plantId) => ({
+  type: "REMOVE_FROM_CART",
+  payload: plantId,
+});
+
+export const increaseQuantity = (plantId) => ({
+  type: "INCREASE_QUANTITY",
+  payload: plantId,
+});
+
+export const decreaseQuantity = (plantId) => ({
+  type: "DECREASE_QUANTITY",
+  payload: plantId,
+});
